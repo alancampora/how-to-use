1bfc3a548841c4bab8f2a05cc894df7be08cb380
Alan Campora <alan.campora@gmail.com>
2017-12-31 12:59:25 -0300

    [ADD] Maybe interface

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,53 @@
 // ------ BASE FP FUNCTIONS
 const compose = (...fns: any[]) =>
     fns.reduce(
-        (prevFn, currentFn) => (data: any) => prevFn(currentFn(data)),
+    (prevFn, currentFn) => (data: any) => prevFn(currentFn(data)),
         (identity: any) => identity
     );
-const Context = (value: any) => ({
-    map: (fn: any) => Context(fn(value))
-});
 
-type Maybe = null | any;
-const Maybe = (value: any) => ({
-    map: (fn: any) =>
-        value === undefined || value == null ? Maybe(null) : Maybe(fn(value)),
-    inspect: `Maybe(${value})`
-});
+interface IMaybe<T> {
+    map <U>( fn: (a:T) => U) : IMaybe<U>;
+}
+
+class Maybe<T> implements IMaybe<T>{
+    private value:T
+
+    constructor(value:T){
+       this.value = value; 
+    }
+    map<U>(fn: (a:T) => U){
+        return new Maybe(fn(this.value))
+    }
+}
+
 // ------ BASE FP FUNCTIONS
 
 // ---------------------------- PROGRAM ----------------------------
 
 // ------ WARNING: NON PURE FUNCTIONS GO HERE
 const readFromData: 
-    (d: examples[]) => (t: string) => Maybe = 
+    (d: example[]) => (t: string) => example[] = 
     data => target => data.filter(entry => entry.keys.indexOf(target) >= 0);
 
 const search: 
-    (t: string) => (d: examples[]) => Maybe =  
-    target => data => Maybe(target).map(readFromData(data));
+    (t: string) => (d: example[]) => IMaybe<example[]> =  
+    target => data => new Maybe(target).map(readFromData(data));
+
+const showOptions: 
+    (o: any[]) => any = 
+    options => console.log(options)
 // ------ WARNING: NON PURE FUNCTIONS GO HERE
 
 const main: 
-    (t: string, d: examples[]) => any =
+    (t: string, d: example[]) => any =
     (target,data) => {
-        let results: Maybe = search(target)(data);
-        console.log(results);
-    };
+        search(target)(data)
+            .map(showOptions)
+    }
 
-interface examples {
+interface example {
     keys: string[];
 }
-
 let dummyTarget: string = 'functional';
-let dummyData: examples[] = [{keys: ['functional', 'map', 'array']}];
+let dummyData: example[] = [{keys: ['functional', 'map', 'array']}];
 main(dummyTarget, dummyData);
