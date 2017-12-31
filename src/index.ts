c29d6d36c012243ec21ec82796202dc028501084
Alan Campora <alan.campora@gmail.com>
2017-12-30 22:10:05 -0300

    [ADD] Trying to get as close as possible to Hindley–Milner

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,44 @@
-import * as os from 'os';
-import {readFile} from 'fs';
-import {add} from './foo/foo';
-
-readFile('./package.json', (err, packageStr) => {
-    if (err) {
-        console.error('There was a problem reading package json', err);
-        return;
-    }
-
-    const json = JSON.parse(packageStr.toString());
-
-    console.log(`Running typescript-node-starter version ${json.version}`);
-    console.info(`Running on ${os.hostname()} with ${os.cpus().length} CPU's and ${os.totalmem()} mem`);
-    console.info(`1 + 1 = ${add(1, 1)}`);
+// ------ BASE FP FUNCTIONS
+const compose = (...fns: any[]) =>
+    fns.reduce(
+        (prevFn, currentFn) => (data: any) => prevFn(currentFn(data)),
+        (identity: any) => identity
+    );
+const Context = (value: any) => ({
+    map: (fn: any) => Context(fn(value))
 });
+
+type Maybe = null | any;
+const Maybe = (value: any) => ({
+    map: (fn: any) =>
+        value === undefined || value == null ? Maybe(null) : Maybe(fn(value)),
+    inspect: `Maybe(${value})`
+});
+// ------ BASE FP FUNCTIONS
+
+// ---------------------------- PROGRAM ----------------------------
+
+// ------ WARNING: NON PURE FUNCTIONS GO HERE
+const readFromData: 
+    (d: examples[]) => (t: string) => Maybe = 
+    data => target => data.filter(entry => entry.keys.indexOf(target) >= 0);
+
+const search: 
+    (t: string, d: examples[]) => Maybe =  
+    (target,data) => Maybe(target).map(readFromData(data));
+// ------ WARNING: NON PURE FUNCTIONS GO HERE
+
+const main: 
+    (t: string, d: examples[]) => any =
+    (target,data) => {
+        let results: Maybe = search(target, data);
+        console.log(results);
+    };
+
+interface examples {
+    keys: string[];
+}
+
+let dummyTarget: string = 'functional';
+let dummyData: examples[] = [{keys: ['functional', 'map', 'array']}];
+main(dummyTarget, dummyData);
