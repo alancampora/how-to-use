dee5c0a84b3ed6308676b11dfeb14b91b2fdaade
Alan Campora <alan.campora@gmail.com>
2018-01-01 17:26:28 -0300

    [ADD] Inquirer and integration with ts

--- src/show.ts
+++ src/show.ts
@@ -1,6 +1,29 @@
+import * as inquirer from 'inquirer'
+import {compose} from './fp-ts'
 import {IExample} from './types'
+
+const createChoices: 
+    (o: IExample[]) => inquirer.ChoiceType[] = 
+    options => options
+                    .map((option,index)=> ({
+                        "message": option.title,
+                        "value": option.title,
+                        "name": option.title
+                    })) 
+
+const createQuestion:
+    (c: inquirer.ChoiceType[]) => inquirer.Question[] = 
+    choices => [({
+        type: "checkbox",
+        message: "Results found",
+        name: "Results",
+        choices: choices
+    })]
 
 export const show: 
     (o: IExample[]) => any = 
-    options => console.log(options);
-
+    options => inquirer
+                .prompt(compose(createQuestion,createChoices)(options))
+                .then((response:any) => console.log(response))
+                .catch((error:any) => console.log(error))
+       
